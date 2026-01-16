e207713ed566cca5b312e468990fa0a78c546bb4
MariusGoeren <test@test.de>
2026-01-16 16:45:01 +0100

    Commit after finishing the project and the presentation

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,60 +1,94 @@
 import random
 
-# Soldier
-
-
+###########################################
+########## Soldier ########################
+###########################################
 class Soldier:
     def __init__(self, health, strength):
-        # your code here
+        self.health = health
+        self.strength = strength
     
     def attack(self):
-        # your code here
+        return self.strength
 
     def receiveDamage(self, damage):
-        # your code here
-    
+        self.health -= damage
 
-# Viking
-
+###########################################
+########## Viking #########################
+###########################################
 class Viking(Soldier):
     def __init__(self, name, health, strength):
-        # your code here
+        super().__init__(health, strength)                                                  # taking the things from PARENT class
+        self.name = name
 
     def battleCry(self):
-        # your code here
+        return "Odin Owns You All!"
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage                                                               # subtract damage taken from actual health
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
 
-# Saxon
-
+###########################################
+########## Saxon ##########################
+###########################################
 class Saxon(Soldier):
     def __init__(self, health, strength):
-        # your code here
+        super().__init__(health, strength)                                                # taking the things from PARENT class
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage                                                               # subtract damage taken from actual health
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
+            return "A Saxon has died in combat"
 
-# Davicente
-
+###########################################
+########## Davicente ######################
+###########################################
 class War():
     def __init__(self):
-        # your code here
+        self.vikingArmy = []                                                                # create an empty list to store the Viking army
+        self.saxonArmy = []                                                                 # create an empty list to store the Saxon army
 
     def addViking(self, viking):
-        # your code here
+        self.vikingArmy.append(viking)                                                      # append a new Instance of the input class to the list
     
     def addSaxon(self, saxon):
-        # your code here
+        self.saxonArmy.append(saxon)                                                        # append a new Instance of the input class to the list
     
     def vikingAttack(self):
-        # your code here
+        defender_saxon = random.choice(self.saxonArmy)                                      # checking which saxon is the defender (random)
+        attacker_viking = random.choice(self.vikingArmy)                                    # checking which viking is the attacker (random)
+
+        return_value = defender_saxon.receiveDamage(attacker_viking.strength)               # updating defenders health
+                                                                                            # -> executing the defenders method .receiveDamage
+
+        if defender_saxon.health <= 0:                                                      # checking if the defender died by the attack
+            self.saxonArmy.remove(defender_saxon)                                           # if so, the defender will be deleted from the list
+        
+        return return_value
     
     def saxonAttack(self):
-        # your code here
+        defender_viking = random.choice(self.vikingArmy)                                    # checking which viking is the defender (random)
+        attacker_saxon = random.choice(self.saxonArmy)                                      # checking which saxon is the attacker (random)
 
+        return_value = defender_viking.receiveDamage(attacker_saxon.strength)               # updating defenders health
+                                                                                            # -> executing the defenders method .receiveDamage
+
+        if defender_viking.health <= 0:                                                     # checking if the defender died by the attack
+            self.vikingArmy.remove(defender_viking)                                         # if so, the defender will be deleted from the list
+        
+        return return_value
+    
     def showStatus(self):
-        # your code here
-    pass
-
-
+        if len(self.saxonArmy) <= 0:                                                        # if the list of Saxons is empty -> Vikings have won
+            return "Vikings have won the war of the century!"
+        elif len(self.vikingArmy) <= 0:
+            return "Saxons have fought for their lives and survive another day..."          # if the list of Vikings is empty -> Saxons have won
+        else:
+            return "Vikings and Saxons are still in the thick of battle."                   # if both lists are still containing soldiers 
+                                                                                            # -> the fight is not over
