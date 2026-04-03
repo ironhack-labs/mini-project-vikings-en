a061ab324fdb546135c922aff4dc138e0bbb506b
Juanalvaroromero <juanalvaroromero@gmail.com>
2026-04-03 10:38:35 +0200

    version2.0

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -5,63 +5,63 @@ import random
 
 class Soldier:
     def __init__(self, health, strength):
-        # your code here
+        # parent class definition 
         self.health = health
         self.strength = strength
-       
+## parent functions       
     def attack(self):
-        return self.strength# your code here
+        return self.strength
 
     def receiveDamage(self, damage):
         self.damage=damage
-        #self.health -= self.damage
         if self.strength<self.damage:
             self.health = 0
         else:
             self.health -= self.damage
-        # your code here
-  
+        
 
 # Viking
 
 class Viking(Soldier):
     def __init__(self, name, health, strength):
-        # your code here
+        # Child class of Soldier parent class
         super().__init__(health,strength)
         self.name=name
 
     def battleCry(self):
-        # your code here
+        
         return "Odin Owns You All!"
 
     def receiveDamage(self, damage):
-        # your code here
+        
         self.damage=damage
         self.health -= self.damage
-        if self.strength<self.damage:
-            #self.health = 0
+        #die or alive?
+        if self.health<0:
+            
             return f"{self.name} has died in act of combat"
             
         else:
-            #self.health -= self.damage
+            
             return f"{self.name} has received {self.damage} points of damage"
        
 
 class Saxon(Soldier):
     def __init__(self, health, strength):
-        # your code here
+        # Child class of Soldier parent class
         super().__init__(health,strength)
 
     def receiveDamage(self, damage):
-        # your code here
+        
         self.damage=damage
         self.health -= self.damage
-        if self.strength<self.damage:
+        #die or alive?
+        if self.health<0:
             #self.health = 0
             return "A Saxon has died in combat"
             
         else:
-            #self.health -= self.damage
+           
             return f"A Saxon has received {self.damage} points of damage"
             
 
@@ -101,19 +101,17 @@ class War():
     
     def saxonAttack(self):
         # your code here
-        if len(self.vikingArmy) == 0 or len(self.saxonArmy) == 0:
-            return "No enough soldiers to attack"
-        else:
-            self.viking=random.choice(self.vikingArmy)
-            self.saxon=random.choice(self.saxonArmy)
-            damage = self.saxon.strength
-            attack= self.viking.receiveDamage(damage) 
+        
+        self.viking=random.choice(self.vikingArmy)
+        self.saxon=random.choice(self.saxonArmy)
+        damage = self.saxon.strength
+        attack= self.viking.receiveDamage(damage) 
 
-            if self.viking.health <= 0:
-                self.vikingArmy.remove(self.viking)   
+        if self.viking.health <= 0:
+            self.vikingArmy.remove(self.viking)   
 
-            return attack
-
+        return attack
+    
     def showStatus(self):
         # your code here
         if len(self.saxonArmy) == 0 and len(self.vikingArmy) > 0:
